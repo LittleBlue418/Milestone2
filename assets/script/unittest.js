e7b620a1e964016d5e6913d61812eb53adcc6b95
LittleBlue418 <hollyathomas88@gmail.com>
2019-10-23 19:51:15 +0200

    Written Jasmine tests for player class

--- assets/script/unittest.js
+++ assets/script/unittest.js
@@ -14,17 +14,45 @@ describe("Player tests", function () {
   });
 
   describe("Player Death", function () {
-    it("Should return false when player health is positive", function() {
+    it("Should return false when player health is positive", function () {
       expect(player.isDead()).toBe(false);
     });
-    it("Should return true when player health is zero", function() {
+    it("Should return true when player health is zero", function () {
       player.health = 0;
       expect(player.isDead()).toBe(true);
     })
-    it("Should return true when player health is negative", function() {
+    it("Should return true when player health is negative", function () {
       player.health = -1;
       expect(player.isDead()).toBe(true);
     })
   });
+
+  describe("Player damage taken", function () {
+    it("Should remove damage ammount from health", function () {
+      player.takeDamage(10);
+      expect(player.health).toBe(90);
+    })
+    it("Player health should be set to zero if the damage is greater than the remaining health", function () {
+      player.takeDamage(101);
+      expect(player.health).toBe(0);
+    })
+  })
+
+  describe("Player drink health potion", function () {
+    it("Should restore health if the player has lost health", function () {
+      player.takeDamage(20);
+      player.drinkPotion(12);
+      expect(player.health).toBe(92);
+    })
+    it("Should only restore up to player max health", function () {
+      player.takeDamage(20);
+      player.drinkPotion(22);
+      expect(player.health).toBe(100);
+    })
+    it("Should not restore health if the player has max health", function () {
+      player.drinkPotion(22);
+      expect(player.health).toBe(100);
+    })
+  })
 
 });
